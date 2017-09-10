545b5b20c12af91d4f3a79161185f2848cc93544
Lawrence Daniels <lawrence.daniels@gmail.com>
2017-09-09 19:30:16 -0700

    added nth largest and smallest methods

--- src/main/scala/com/github/ldaniels528/interview/scalaprep/BinaryTree.scala
+++ src/main/scala/com/github/ldaniels528/interview/scalaprep/BinaryTree.scala
@@ -7,14 +7,15 @@ object BinaryTree extends App {
 
   val random = new Random()
   val tree = new BSTree[Integer]()
-  for(_ <- 1 to 10) tree.add(random.nextInt(100))
-  tree.ascending() foreach(v => println(v))
+  for (_ <- 1 to 10) tree.add(random.nextInt(100))
+  tree.ascending() foreach (v => println(v))
   println("*" * 20)
-  tree.descending() foreach(v => println(v))
+  tree.descending() foreach (v => println(v))
   println("*" * 20)
   println(s"max: ${tree.max()}")
   println(s"min: ${tree.min()}")
-
+  println(s"3rd largest: ${tree.nthLargest(3)}")
+  println(s"3rd smallest: ${tree.nthSmallest(3)}")
 
   class BSTree[T <: Comparable[T]]() {
     private var root: BSTNode[T] = _
@@ -23,12 +24,12 @@ object BinaryTree extends App {
       if (root == null) root = BSTNode(value) else append(value, root)
     }
 
-    def ascending(node: BSTNode[T] = root): List[T] = {
-      if (node == null) Nil else ascending(node.left) ::: node.value :: ascending(node.right)
+    def ascending(node: BSTNode[T] = root): Stream[T] = {
+      if (node == null) Stream.empty else ascending(node.left) #::: node.value #:: ascending(node.right)
     }
 
-    def descending(node: BSTNode[T] = root): List[T] = {
-      if (node == null) Nil else descending(node.right) ::: node.value :: descending(node.left)
+    def descending(node: BSTNode[T] = root): Stream[T] = {
+      if (node == null) Stream.empty else descending(node.right) #::: node.value #:: descending(node.left)
     }
 
     def max(node: BSTNode[T] = root): Option[T] = {
@@ -37,6 +38,16 @@ object BinaryTree extends App {
 
     def min(node: BSTNode[T] = root): Option[T] = {
       if (node == null) None else min(node.left) ?? Option(node.value) ?? min(node.right)
+    }
+
+    def nthLargest(nth: Int): Option[T] = {
+      val list = descending().take(nth)
+      if (list.size < nth) None else list.lastOption
+    }
+
+    def nthSmallest(nth: Int): Option[T] = {
+      val list = ascending().take(nth)
+      if (list.size < nth) None else list.lastOption
     }
 
     @tailrec
@@ -59,7 +70,7 @@ object BinaryTree extends App {
   final implicit class OptionalMagic[A](val opt: Option[A]) extends AnyVal {
 
     @inline
-    def ??(optB: Option[A]): Option[A] = if(opt.isEmpty) optB else opt
+    def ??(optB: Option[A]): Option[A] = if (opt.isEmpty) optB else opt
 
   }
 
